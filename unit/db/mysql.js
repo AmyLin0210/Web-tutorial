38be24bf6395b49a56fc280f35e8fc82a296cf0d
林謙慈 <amylin19990210@gmail.com>
2019-04-16 16:46:06 +0800

    QAQ

--- unit/db/mysql.js
+++ unit/db/mysql.js
@@ -14,7 +14,9 @@ connection.connect(err => {
 
 // Step 2
 // create table if necessary
-
+connection.query('CREATE TABLE IF NOT EXISTS student (id VARCHAR(10), name VARCHAR(30), cid VARCHAR(30))')
+connection.query('CREATE TABLE IF NOT EXISTS course (id VARCHAR(10), name VARCHAR(30))')
+connection.query('CREATE TABLE IF NOT EXISTS score (id VARCHAR(10), score VARCHAR(10))')
 
 // show table
 
